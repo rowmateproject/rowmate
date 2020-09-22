60563c9818ac69db2998c3382cfa3667197664b5
maurosbicego <mauro@sbicego.ch>
2020-09-22 21:47:03 +0200

    Redirect to Dashboard when user is logged in

--- app/middleware/notAuthenticated.js
+++ app/middleware/notAuthenticated.js
@@ -1,6 +1,6 @@
 export default function({ store, redirect }) {
-  // If the user is authenticated redirect to home page
+  // If the user is authenticated redirect to dashboard
   if (store.state.accessToken) {
-    return redirect('/')
+    return redirect('/dashboard')
   }
 }
