c1d5bc6f39fdb55a235861b58f59cd9818f19a32
Aurelius Wendelken <webtobesocial@gmail.com>
2020-09-24 10:47:31 +0200

    improved usability in sidebar when icons where not some size with grid colomns

--- app/components/account/sidebar.vue
+++ app/components/account/sidebar.vue
@@ -1,50 +1,39 @@
 <template>
 <div class="flex min-h-screen">
-  <div :class="isOpen ? 'block' : 'hidden'" @click="isOpen = false" class="fixed z-20 inset-0 bg-black opacity-50 transition-opacity lg:hidden"></div>
-
-  <div :class="isOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'" class="fixed z-30 inset-y-0 left-0 w-64 transition duration-300 transform bg-gray-800 overflow-y-auto lg:translate-x-0 lg:static lg:inset-0">
-    <div :class="isSuperuser === true ? 'bg-red-900' : 'bg-gray-900'" class="flex items-center justify-center pt-6 pb-6">
-      <div class="flex items-center">
-        <fa :icon="['fas', 'id-card']" class="text-gray-250 text-2xl" />
-        <span class="text-white text-2xl mx-2 font-semibold">rowmate.org</span>
-      </div>
+  <div class="fixed z-30 inset-y-0 left-0 w-64 transition duration-300 transform -translate-x-full ease-in bg-gray-800 overflow-y-auto lg:translate-x-0 lg:static lg:inset-0">
+    <div :class="isSuperuser ? 'bg-blue-550' : 'bg-gray-900'" class="text-center py-5">
+        <span class="text-white text-3xl mx-2 font-semibold">rowmate.org</span>
     </div>
 
     <nav class="mt-2">
-      <nuxt-link class="flex items-center mt-1 py-3 px-6" :class="[comparePageName('dashboard') ? activeClass : inactiveClass]" :to="localePath('/dashboard')" v-if="isSuperuser">
-        <fa :icon="['fas', 'chart-line']" class="text-gray-250" />
-
-        <span class="mx-4">Dashboard</span>
+      <nuxt-link class="grid grid-cols-6 mt-1 py-3 px-6" :class="[comparePageName('dashboard') ? activeClass : inactiveClass]" :to="localePath('/dashboard')" v-if="isSuperuser">
+        <fa :icon="['fas', 'chart-line']" class="col-span-1 text-gray-250" />
+        <span class="col-span-5">Dashboard</span>
       </nuxt-link>
 
-      <nuxt-link class="flex items-center mt-1 py-3 px-6" :class="[comparePageName('settings') ? activeClass : inactiveClass]" :to="localePath('/settings')">
-        <fa :icon="['fas', 'server']" class="text-gray-250" />
-
-        <span class="mx-4">Einstellungen</span>
+      <nuxt-link class="grid grid-cols-6 mt-1 py-3 px-6" :class="[comparePageName('settings') ? activeClass : inactiveClass]" :to="localePath('/settings')">
+        <fa :icon="['fas', 'user-cog']" class="col-span-1 text-gray-250" />
+        <span class="col-span-5">Einstellungen</span>
       </nuxt-link>
 
-      <nuxt-link class="flex items-center mt-1 py-3 px-6" :class="[comparePageName('users') ? activeClass : inactiveClass]" :to="localePath('/users')">
-        <fa :icon="['fas', 'users']" class="text-gray-250" />
-
-        <span class="mx-4">Nutzer</span>
+      <nuxt-link class="grid grid-cols-6 mt-1 py-3 px-6" :class="[comparePageName('users') ? activeClass : inactiveClass]" :to="localePath('/users')">
+        <fa :icon="['fas', 'users']" class="col-span-1 text-gray-250" />
+        <span class="col-span-5">Nutzer</span>
       </nuxt-link>
 
-      <nuxt-link class="flex items-center mt-1 py-3 px-6" :class="[comparePageName('xxx') ? activeClass : inactiveClass]" :to="localePath('/#')">
-        <fa :icon="['fas', 'comment-alt']" class="text-gray-250" />
-
-        <span class="mx-4">Kontakt</span>
+      <nuxt-link class="grid grid-cols-6 mt-1 py-3 px-6" :class="[comparePageName('xxx') ? activeClass : inactiveClass]" :to="localePath('/#')">
+        <fa :icon="['fas', 'comment-alt']" class="col-span-1 text-gray-250" />
+        <span class="col-span-5">Kontakt</span>
       </nuxt-link>
 
-      <nuxt-link class="flex items-center mt-1 py-3 px-6" :class="[comparePageName('xxx') ? activeClass : inactiveClass]" :to="localePath('/#')">
-        <fa :icon="['fas', 'calendar-week']" class="text-gray-250" />
-
-        <span class="mx-4">Termine</span>
+      <nuxt-link class="grid grid-cols-6 mt-1 py-3 px-6" :class="[comparePageName('xxx') ? activeClass : inactiveClass]" :to="localePath('/#')">
+        <fa :icon="['fas', 'calendar-week']" class="col-span-1 text-gray-250" />
+        <span class="col-span-5">Termine</span>
       </nuxt-link>
 
-      <nuxt-link class="flex items-center mt-1 py-3 px-6" :class="[comparePageName('xxx') ? activeClass : inactiveClass]" :to="localePath('/#')">
-        <fa :icon="['fas', 'sticky-note']" class="text-gray-250" />
-
-        <span class="mx-4">Verwaltung</span>
+      <nuxt-link class="grid grid-cols-6 mt-1 py-3 px-6" :class="[comparePageName('xxx') ? activeClass : inactiveClass]" :to="localePath('/#')">
+        <fa :icon="['fas', 'sticky-note']" class="col-span-1 text-gray-250" />
+        <span class="col-span-5">Verwaltung</span>
       </nuxt-link>
     </nav>
   </div>
@@ -55,7 +44,6 @@
 export default {
   data() {
     return {
-      isOpen: true,
       activeClass: 'bg-gray-600 bg-opacity-25 text-gray-100 border-gray-100',
       inactiveClass: 'border-gray-900 text-gray-500 hover:bg-gray-600 hover:bg-opacity-25 hover:text-gray-100'
     }
