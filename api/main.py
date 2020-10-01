4bed689f97ba091f3d974d42d22fe07722ad1cbe
Aurelius Wendelken <webtobesocial@gmail.com>
2020-10-01 23:53:33 +0200

    implemented mehtod to update or delete or insert an existing event

--- api/main.py
+++ api/main.py
@@ -719,47 +719,117 @@ async def patch_event(hex: str,
                       request: UpdateEvent,
                       user=Depends(api_user.get_current_superuser)):
     try:
-        uuid = UUID(hex=hex, version=4)
+        req_uuid = UUID(hex=hex, version=4)
     except TypeError:
         raise HTTPException(status_code=404, detail='Event was not found')
 
-    query = {'events': {'$in': [uuid]}}
+    query = {'events': {'$in': [req_uuid]}}
     res = await db['calendars'].find(query).to_list(length=1)
 
+    if len(res) == 0:
+        raise HTTPException(status_code=404, detail='Calender was not found')
+
     updated = []
+    inserted = []
+    deleted = []
 
-    if len(res) > 0:
-        for uuid in res[0]['events']:
-            request_doc = dict(request)
-            modified_at: datetime = datetime.utcnow()
+    event_times = generate_interval(request)
+    existing_events = len(res[0]['events'])
+    interval_events = len(event_times)
 
-            event_doc: Event = {
-                'modified_by': user.name,
-                'modified_at': modified_at,
-                'titles': request_doc['titles'],
-                'location': request_doc['location'],
-                'event_time': request_doc['event_time'],
-                'repeat_unit': request_doc['repeat_unit'],
-                'descriptions': request_doc['descriptions'],
-                'repeat_interval': request_doc['repeat_interval'],
-                'min_participants': request_doc['min_participants'],
-                'max_participants': request_doc['max_participants'],
-                'contact_person': request_doc['contact_person'],
-                'start_time': request_doc['start_time'],
-                'end_time': request_doc['end_time']
-            }
+    for event_time_index, event_time in enumerate(event_times):
+        modified_at: datetime = datetime.utcnow()
+        request_doc = dict(request)
 
-            print(event_doc)
+        event_doc: Event = {
+            'event_time': event_time,
+            'titles': request_doc['titles'],
+            'location': request_doc['location'],
+            'repeat_unit': request_doc['repeat_unit'],
+            'descriptions': request_doc['descriptions'],
+            'repeat_interval': request_doc['repeat_interval'],
+            'min_participants': request_doc['min_participants'],
+            'max_participants': request_doc['max_participants'],
+            'contact_person': request_doc['contact_person'],
+            'start_time': request_doc['start_time'],
+            'end_time': request_doc['end_time']
+        }
 
-            query = {'_id': uuid}
+        if existing_events > 0:
+            event_doc['modified_by'] = user.name
+            event_doc['modified_at'] = modified_at
+
             document = {'$set': event_doc}
+            query = {'_id': {'$in': [res[0]['events'][event_time_index]]}}
 
-            res = await db['events'].update_one(query, document, upsert=True)
+            update_res = await db['events'].update_many(query, document)
 
-            if res.acknowledged:
-                updated.append(uuid)
+            if update_res.acknowledged:
+                updated.append(res[0]['events'][event_time_index])
+        elif existing_events < 0 and existing_events < interval_events:
+            created_at: datetime = datetime.utcnow()
+            uuid: Binary = generate_uuid()
+
+            event_doc['_id'] = uuid
+            event_doc['author'] = user.name
+            event_doc['created_at'] = created_at
+
+            insert_res = await db['events'].insert_one(event_doc)
+
+            if insert_res.acknowledged:
+                inserted.append(insert_res.inserted_id)
+
+        existing_events = existing_events - 1
+    interval_events = interval_events - 1
+
+    delete_uuids = set(res[0]['events']) - set(updated)
+
+    if len(delete_uuids) > 0:
+        for uuid in delete_uuids:
+            query = {'_id': uuid}
+
+            delete_res = await db['events'].delete_one(query)
+
+            if delete_res.deleted_count > 0:
+                deleted.append(uuid)
+
+    if len(inserted) > 0:
+        calendar_doc: Calendar = {
+            'start_time': request_doc['start_time'],
+            'end_time': request_doc['end_time']
+        }
+
+        query = {'events': {'$in': [req_uuid]}}
+        document = {'$set': calendar_doc,
+                    '$addToSet': {'events': {'$each': inserted}}}
+
+        await db['calendars'].update_one(query, document)
 
     if len(updated) > 0:
-        return {'detail': f'Updated {len(updated)} events'}
+        calendar_doc: Calendar = {
+            'start_time': request_doc['start_time'],
+            'end_time': request_doc['end_time']
+        }
+
+        query = {'events': {'$in': [req_uuid]}}
+        document = {'$set': calendar_doc}
+
+        await db['calendars'].update_one(query, document)
+
+    if len(deleted) > 0:
+        calendar_doc: Calendar = {
+            'start_time': request_doc['start_time'],
+            'end_time': request_doc['end_time']
+        }
+
+        query = {'events': {'$in': [req_uuid]}}
+        document = {'$pullAll': {'events': deleted}}
+
+        await db['calendars'].update_one(query, document)
+
+    if len(updated) > 0 or len(inserted) > 0 or len(deleted) > 0:
+        return {'updated': len(updated),
+                'inserted': len(inserted),
+                'deleted': len(deleted)}
     else:
-        raise HTTPException(status_code=400, detail='Could not update events')
+        raise HTTPException(status_code=400, detail='Error updating calendar')
