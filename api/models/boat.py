0b89015d54312c3ddfce00409323558cb3a99318
Aurelius Wendelken <webtobesocial@gmail.com>
2020-10-03 09:14:22 +0200

    fixed issue not creating default binary uuids, fastapi issue #2123

--- api/models/boat.py
+++ api/models/boat.py
@@ -1,8 +1,7 @@
-from bson.binary import Binary, UUID_SUBTYPE
+from bson.binary import Binary
 from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
-from uuid import uuid4
 
 # enums
 from enums.boatcategory import BoatCategoryEnum
@@ -11,7 +10,7 @@ from enums.coxswain import CoxswainEnum
 
 
 class Boat(BaseModel):
-    id: Binary = Binary(bytes(bytearray(uuid4().bytes)), UUID_SUBTYPE)
+    id: Binary
     name: str
     crewsize: int
     added: datetime = datetime.utcnow()
--- api/models/template.py
+++ api/models/template.py
@@ -1,11 +1,10 @@
-from bson.binary import Binary, UUID_SUBTYPE
+from bson.binary import Binary
 from pydantic import BaseModel
 from typing import Optional
-from uuid import uuid4
 
 
 class TemplateModel(BaseModel):
-    id: Binary = Binary(bytes(bytearray(uuid4().bytes)), UUID_SUBTYPE)
+    id: Binary
     locale: Optional[str]
     topic: Optional[str]
     subject: str
