bf63fd33c9d198874875e7aa84de45d9907376fc
Aurelius Wendelken <webtobesocial@gmail.com>
2020-10-11 11:08:04 +0200

    fixed broken boats router endpoint address

--- api/router/boat.py
+++ api/router/boat.py
@@ -9,7 +9,7 @@ from models.boat import Boat
 def get_boats_router(database, authenticator) -> APIRouter:
     router = APIRouter()
 
-    @router.get('/')
+    @router.get('')
     async def get_all_boats(user=Depends(
             authenticator.get_current_active_user)):
         sort = [('_id', pymongo.DESCENDING)]
@@ -29,7 +29,7 @@ def get_boats_router(database, authenticator) -> APIRouter:
 def get_boat_router(database, authenticator) -> APIRouter:
     router = APIRouter()
 
-    @router.post('/')
+    @router.post('')
     async def post_boat(boat: Boat, user=Depends(
             authenticator.get_current_superuser)):
         res = await database['boats'].insert_one(dict(boat))
