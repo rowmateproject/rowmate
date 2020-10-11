93bce87145841bc0c1a18a56ac90ee61552f012d
Aurelius Wendelken <webtobesocial@gmail.com>
2020-10-11 02:59:18 +0200

    fixed a few linting issues which caused app to stop working

--- app/plugins/boatcategory.js
+++ app/plugins/boatcategory.js
@@ -1,33 +1,34 @@
 class BoatCategory {
   static getCategoryString(b) {
-    let boatstr = ""
-    if (b.category == 0) {
-      boatstr += "G"
+    let boatstr = ''
+
+    if (b.category === 0) {
+      boatstr += 'G'
     }
+
     if (b.coxswain === 0 || b.coxswain === 3) {
-      boatstr += (b.crewsize-1).toString()
+      boatstr += (b.crewsize - 1).toString()
     } else {
       boatstr += b.crewsize.toString()
     }
 
-    if (b.discipline == 0 || b.discipline == 2) {
-      boatstr += "x"
+    if (b.discipline === 0 || b.discipline === 2) {
+      boatstr += 'x'
     }
 
-    if (b.coxswain == 0 || b.coxswain == 3) {
-      boatstr += "+"
+    if (b.coxswain === 0 || b.coxswain === 3) {
+      boatstr += '+'
     }
-
-
-
 
     return boatstr
   }
+
   static getCategories(boats) {
-    let categories = {}
+    const categories = {}
+
     boats.forEach((boat, i) => {
       categories[boat.id] = this.getCategoryString(boat)
-    });
+    })
 
     return categories
   }
