a4e01393887165b0adad3cd155da9e55bb32c193
Aurelius Wendelken <webtobesocial@gmail.com>
2020-10-17 01:37:31 +0200

    add allow origins url from settings as frontend url on runtime

--- api/asgi.py
+++ api/asgi.py
@@ -34,7 +34,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000'],
+    allow_origins=[settings.frontend_url],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*']
